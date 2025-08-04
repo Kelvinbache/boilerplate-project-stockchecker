a8dff3cba0ccf495ddbc53c55e277787f8b7276b
kelvin abche <kelvinabache12@gmail.com>
2025-08-04 17:31:17 -0400

    update

--- routes/api.js
+++ routes/api.js
@@ -28,6 +28,7 @@ module.exports = function (app) {
       } else {
         res.json({ stockData });
       }
+      
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -11,11 +11,72 @@ suite('Functional Tests', function() {
     chai.request(server)
       .get('/api/stock-prices?stock=GOOGL')
       .end(function(err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.property(res.body, 'stockData');
-        assert.isArray(res.body.stockData);
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        
+        // get the stock data
+        expect(res.body.stockData).to.have.property("stock").that.is.a('string');
         done();
+});
+ 
+test('GET /api/stock-prices => stock data with likes', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOGL&like=true')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+
+        // likes the stock
+        expect(res.body.stockData).to.have.property("likes").to.equal(1);
+      done();
+      });
+    });
+
+test('GET /api/stock-prices => stock data with likes and multiple stocks', function(done) {
+        chai.request(server)
+        .get('/api/stock-prices?stock=GOOGL&like=true')
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+
+        // view the stock and likes
+        expect(res.body.stockData).to.have.property("stock").that.is.a('string');
+        expect(res.body.stockData).to.have.property("likes").to.equal(2);
+
+        done();
+        });
+      });
+
+
+      
+test('GET /api/stock-prices => stock data with likes and multiple stocks', function(done) {
+        chai.request(server)
+        .get('/api/stock-prices?stock=GOOGL&like=true')
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+        //  two stocks
+        expect(res.body).to.have.property("stockData").that.is.an('array').with.lengthOf(2);
+        done();
+
+        });
+      });
+
+
+
+      
+test('GET /api/stock-prices => stock data with likes and multiple stocks', function(done) {
+        chai.request(server)
+        .get('/api/stock-prices?stock=GOOGL&like=true')
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+
+        
+        // two stocks with likes
+        res.body.stockData.forEach(stock => {
+          expect(stock).to.have.property("stock").that.is.a('string');
+          expect(stock).to.have.property("rel_likes").to.equal(2);
+        });
+
+         done();
+        });
       });
   });
-});
+});  
